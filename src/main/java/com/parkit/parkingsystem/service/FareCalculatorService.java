baf199ef104405e51b237ecbb4c07069713f5bf7
gtn777 <gtn777@orange.fr>
2022-02-26 04:57:56 +0100

    reinitialisation project

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,27 +6,23 @@ import com.parkit.parkingsystem.model.Ticket;
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
-    	
-    	if (ticket.getOutTime().before(ticket.getInTime())) {
-            throw new IllegalArgumentException("Out time provided is before in time:"+ticket.getOutTime().toString());
-    	}
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        long inMilliseconds = ticket.getInTime().getTime();
-        long outMilliseconds= ticket.getOutTime().getTime();
+        int inHour = ticket.getInTime().getHours();
+        int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        long duration =  (outMilliseconds - inMilliseconds) ;
+        int duration = outHour - inHour;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR)/ 3600000);
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR)/ 3600000);
+                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,12 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,99 +13,53 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.parkit.parkingsystem.dao.ParkingSpotDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
-import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
-import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
-import com.parkit.parkingsystem.service.ParkingService;
-import com.parkit.parkingsystem.util.InputReaderUtil;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
-	private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
-	private static ParkingSpotDAO parkingSpotDAO;
-	private static TicketDAO ticketDAO;
-	private static DataBasePrepareService dataBasePrepareService;
+    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static ParkingSpotDAO parkingSpotDAO;
+    private static TicketDAO ticketDAO;
+    private static DataBasePrepareService dataBasePrepareService;
 
-	@Mock
-	private static InputReaderUtil inputReaderUtil;
+    @Mock
+    private static InputReaderUtil inputReaderUtil;
 
-	@BeforeAll
-	private static void setUp() throws Exception {
-		parkingSpotDAO = new ParkingSpotDAO();
-		parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
-		ticketDAO = new TicketDAO();
-		ticketDAO.dataBaseConfig = dataBaseTestConfig;
-		dataBasePrepareService = new DataBasePrepareService();
-	}
+    @BeforeAll
+    private static void setUp() throws Exception{
+        parkingSpotDAO = new ParkingSpotDAO();
+        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+        ticketDAO = new TicketDAO();
+        ticketDAO.dataBaseConfig = dataBaseTestConfig;
+        dataBasePrepareService = new DataBasePrepareService();
+    }
 
-	@BeforeEach
-	private void setUpPerTest() throws Exception {
-		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("pdbIT");
-		dataBasePrepareService.clearDataBaseEntries();
-	}
+    @BeforeEach
+    private void setUpPerTest() throws Exception {
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        dataBasePrepareService.clearDataBaseEntries();
+    }
 
-	@AfterAll
-	private static void tearDown() {
-//		dataBasePrepareService.clearDataBaseEntries();
-	}
+    @AfterAll
+    private static void tearDown(){
 
-	@Test
-	public void testParkingACar() {
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingService.processIncomingVehicle();
-		// TODO: check that a ticket is actually saved in DB and Parking table is
-		// updated with availability
-		int savedUnavailableParkingNumber = -1;
-		try {
-			Connection con = dataBaseTestConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement("SELECT * FROM parking WHERE AVAILABLE = 0");
-			ResultSet rs = ps.executeQuery();
-			if (rs.next() && rs.isLast()) {
-				savedUnavailableParkingNumber = rs.getInt(1);
-			} else {
-				System.out.println("table parking multiple or no entry, ParkingDataBaseIT.testParkingACAR \n");
-			}
-			dataBaseTestConfig.closeResultSet(rs);
-			dataBaseTestConfig.closePreparedStatement(ps);
-			dataBaseTestConfig.closeConnection(con);
-		} catch (Exception e) {
-			System.out.println("Error fetching parking table, ParkingDataBaseIT.testParkingCar \n" + e);
-		}
+    }
 
-		assertEquals(1, savedUnavailableParkingNumber);
-	}
+    @Test
+    public void testParkingACar(){
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+    }
 
-	@Test
-	public void testParkingLotExit() {
-		testParkingACar();
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingService.processExitingVehicle();
-		// TODO: check that the fare generated and out time are populated correctly in
-		// the database
-
-		String savedRegNumber = "";
-		try {
-			Connection con = dataBaseTestConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement("SELECT * FROM ticket WHERE PRICE > 0 AND OUT_TIME > 0	");
-			ResultSet rs = ps.executeQuery();
-			if (rs.next() && rs.isLast()) {
-				savedRegNumber = rs.getString(3);
-			} else {
-				System.out.println("table ticket multiple or no entry, ParkingDataBaseIT.testParkingLotExit \n");
-			}
-			dataBaseTestConfig.closeResultSet(rs);
-			dataBaseTestConfig.closePreparedStatement(ps);
-			dataBaseTestConfig.closeConnection(con);
-		} catch (Exception e) {
-			System.out.println("Error fetching parking ticket, ParkingDataBaseIT.testParkingLotExit \n" + e);
-		}
-
-		assertEquals("pdbIT", savedRegNumber);
-
-	}
+    @Test
+    public void testParkingLotExit(){
+        testParkingACar();
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processExitingVehicle();
+        //TODO: check that the fare generated and out time are populated correctly in the database
+    }
 
 }
