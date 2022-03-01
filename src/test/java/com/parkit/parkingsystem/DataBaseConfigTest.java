9bec6a8dd7042b95b42c5c3bae48c93b74d7791a
gtn777 <gtn777@orange.fr>
2022-03-01 01:27:13 +0100

    DataBaseConfigTest up

--- src/test/java/com/parkit/parkingsystem/DataBaseConfigTest.java
+++ src/test/java/com/parkit/parkingsystem/DataBaseConfigTest.java
@@ -1,7 +1,14 @@
 package com.parkit.parkingsystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,7 +19,6 @@ import org.junit.jupiter.api.Test;
 import com.parkit.parkingsystem.config.DataBaseConfig;
 
 class DataBaseConfigTest {
-	private static DataBaseConfig dataBaseConfig;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -30,15 +36,35 @@ class DataBaseConfigTest {
 	void tearDown() throws Exception {
 	}
 
-	@Disabled
-	@Test
-	void test1() {
-		fail("Not yet implemented");
-	}
+	private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
+	@Test
+	void userAccessToSqlDatabaseTest(){
+		// GIVEN
+		DataBaseConfig dataBaseConfig = new DataBaseConfig();
+		Connection con = null;
+		Boolean isConnected = null;
+		Boolean isConnectionClosed = null;
+		// WHEN
+		try {
+			con = dataBaseConfig.getConnection();
+			isConnected = con.isValid(500);
+			dataBaseConfig.closeConnection(con);
+			isConnectionClosed = !con.isValid(500);
+		} catch (ClassNotFoundException | SQLException e) {
+			logger.error(e.getMessage());
+			logger.error("Access to database impossible, check user access or database existing");
+		}
+		
+		// THEN
+		assertInstanceOf(Connection.class, con);
+		assertEquals(true, isConnected);	
+		assertEquals(true, isConnectionClosed);
+	}
+	
 	@Disabled
 	@Test
-	void test2() {
+	void sqlRequestStatementTest() {
 		fail("Not yet implemented");
 	}
 
