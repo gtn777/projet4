a00a832be0a192bfcff44c3a5d0ee6c8a01268eb
gtn777 <gtn777@orange.fr>
2022-03-08 17:28:45 +0100

    bugs fixing update

---
+++ src/main/java/com/parkit/parkingsystem/config/DBConfigData.java
@@ -0,0 +1,7 @@
+package com.parkit.parkingsystem.config;
+
+public class DBConfigData {
+	public static final String URL = "jdbc:mysql://localhost:3306/parkingprod";
+	public static final String USER = "parking";
+	public static final String PASSWORD = "Parking*1912";
+}
--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -11,48 +11,47 @@ import org.apache.logging.log4j.Logger;
 
 public class DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+	private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/parkingprod","parking","Parking*1912");
-    }
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		logger.info("Create DB connection");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(DBConfigData.URL, DBConfigData.USER, DBConfigData.PASSWORD);
+	}
 
-    public void closeConnection(Connection con){
-        if(con!=null){
-            try {
+	public void closeConnection(Connection con) {
+		if (con != null) {
+			try {
 				con.close();
-                logger.info("Closing DB connection");
-            } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+				logger.info("Closing DB connection");
+			} catch (SQLException e) {
+				logger.error("Error while closing connection", e);
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 
-    public void closePreparedStatement(PreparedStatement ps) {
-        if(ps!=null){
-            try {
-                ps.close();
-                logger.info("Closing Prepared Statement");
-            } catch (SQLException e) {
-                logger.error("Error while closing prepared statement",e);
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+	public void closePreparedStatement(PreparedStatement ps) {
+		if (ps != null) {
+			try {
+				ps.close();
+				logger.info("Closing Prepared Statement");
+			} catch (SQLException e) {
+				logger.error("Error while closing prepared statement", e);
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 
-    public void closeResultSet(ResultSet rs) {
-        if(rs!=null){
-            try {
-                rs.close();
-                logger.info("Closing Result Set");
-            } catch (SQLException e) {
-                logger.error("Error while closing result set",e);
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+	public void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+				logger.info("Closing Result Set");
+			} catch (SQLException e) {
+				logger.error("Error while closing result set", e);
+				System.out.println(e.getMessage());
+			}
+		}
+	}
 }
